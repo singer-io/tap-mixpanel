e1b702939bfc998bcb145baa199b3d81073d9a28
Jeff Huth <39202799+jeffhuth-bytecode@users.noreply.github.com>
2020-04-06 08:53:45 -0700

    v.0.0.3 fix client timeout errors (#1)
    Adjust `client.py` to add better error handling for read timeouts.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.0.3
+  * Adjust `client.py` to add better error handling for read timeouts.
+
 ## 0.0.2
   * Change `export` to streaming. Required changes to client, sync, and transform.
 
--- tap_mixpanel/client.py
+++ tap_mixpanel/client.py
@@ -9,6 +9,14 @@ import singer
 
 LOGGER = singer.get_logger()
 
+# Timeout reference: https://developer.mixpanel.com/docs/jql-api-reference#section-api-limits
+# Queries will timeout after 2 minutes of runtime
+TIMEOUT_SECS = 120
+
+
+class ReadTimeoutError(Exception):
+    pass
+
 
 class Server5xxError(Exception):
     pass
@@ -101,7 +109,7 @@ class MixpanelClient(object):
 
 
     @backoff.on_exception(backoff.expo,
-                          Server5xxError,
+                          (Server5xxError, Server429Error, ReadTimeoutError, ConnectionError),
                           max_tries=5,
                           factor=2)
     def check_access(self):
@@ -116,9 +124,14 @@ class MixpanelClient(object):
         headers['Authorization'] = 'Basic {}'.format(
             str(base64.urlsafe_b64encode(self.__api_secret.encode("utf-8")), "utf-8"))
 
-        response = self.__session.get(
-            url=url,
-            headers=headers)
+        try:
+            response = self.__session.get(
+                url=url,
+                headers=headers,
+                timeout=TIMEOUT_SECS)
+        except requests.exceptions.Timeout as err:
+            LOGGER.error('TIMEOUT ERROR: {}'.format(err))
+            raise ReadTimeoutError
         if response.status_code != 200:
             LOGGER.error('Error status_code = {}'.format(response.status_code))
             raise_for_error(response)
@@ -127,7 +140,7 @@ class MixpanelClient(object):
 
 
     @backoff.on_exception(backoff.expo,
-                          (Server5xxError, ConnectionError, Server429Error),
+                          (Server5xxError, Server429Error, ReadTimeoutError, ConnectionError),
                           max_tries=7,
                           factor=3)
     def request(self, method, url=None, path=None, params=None, json=None, **kwargs):
@@ -159,11 +172,16 @@ class MixpanelClient(object):
         kwargs['headers']['Authorization'] = 'Basic {}'.format(
             str(base64.urlsafe_b64encode(self.__api_secret.encode("utf-8")), "utf-8"))
         with metrics.http_request_timer(endpoint) as timer:
-            response = self.__session.request(method=method,
-                                              url=url,
-                                              params=params,
-                                              json=json,
-                                              **kwargs)
+            try:
+                response = self.__session.request(method=method,
+                                                url=url,
+                                                params=params,
+                                                json=json,
+                                                timeout=TIMEOUT_SECS,
+                                                **kwargs)
+            except requests.exceptions.Timeout as err:
+                LOGGER.error('TIMEOUT ERROR: {}'.format(err))
+                raise ReadTimeoutError
             timer.tags[metrics.Tag.http_status_code] = response.status_code
 
         if response.status_code >= 500:
@@ -177,7 +195,7 @@ class MixpanelClient(object):
 
 
     @backoff.on_exception(backoff.expo,
-                          (Server5xxError, ConnectionError, Server429Error),
+                          (Server5xxError, Server429Error, ReadTimeoutError, ConnectionError),
                           max_tries=7,
                           factor=3)
     def request_export(self, method, url=None, path=None, params=None, json=None, **kwargs):
@@ -209,31 +227,34 @@ class MixpanelClient(object):
         kwargs['headers']['Authorization'] = 'Basic {}'.format(
             str(base64.urlsafe_b64encode(self.__api_secret.encode("utf-8")), "utf-8"))
         with metrics.http_request_timer(endpoint) as timer:
-            with self.__session.request(method=method,
-                                        url=url,
-                                        params=params,
-                                        json=json,
-                                        stream=True,
-                                        timeout=180,
-                                        **kwargs) as response:
-
-                if response.status_code >= 500:
-                    raise Server5xxError()
-
-                if response.status_code != 200:
-                    raise_for_error(response)
-
-                # export endpoint returns jsonl results; 
-                #  other endpoints return json with array of results
-                #  jsonlines reference: https://jsonlines.readthedocs.io/en/latest/
-
-                if response.text == '':
-                    LOGGER.warning('/export API response empty')
-                    yield None
-                else:
-                    file_like_object = io.StringIO(response.text)
-                    reader = jsonlines.Reader(file_like_object)
-                    for record in reader.iter(allow_none=True, skip_empty=True):
-                        yield record
-
+            try:
+                response = self.__session.request(method=method,
+                                            url=url,
+                                            params=params,
+                                            json=json,
+                                            stream=True,
+                                            timeout=TIMEOUT_SECS,
+                                            **kwargs)
+            except requests.exceptions.Timeout as err:
+                LOGGER.error('TIMEOUT ERROR: {}'.format(err))
+                raise ReadTimeoutError
             timer.tags[metrics.Tag.http_status_code] = response.status_code
+
+        if response.status_code >= 500:
+            raise Server5xxError()
+
+        if response.status_code != 200:
+            raise_for_error(response)
+
+        # export endpoint returns jsonl results; 
+        #  other endpoints return json with array of results
+        #  jsonlines reference: https://jsonlines.readthedocs.io/en/latest/
+
+        if response.text == '':
+            LOGGER.warning('/export API response empty')
+            yield None
+        else:
+            file_like_object = io.StringIO(response.text)
+            reader = jsonlines.Reader(file_like_object)
+            for record in reader.iter(allow_none=True, skip_empty=True):
+                yield record
