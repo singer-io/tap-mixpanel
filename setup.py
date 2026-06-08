7709f492a1d08df64ae54aae422770614a029a2a
Akarsh Kumar <akarsh.kumar@qlik.com>
2026-06-08 18:38:28 +0530

    Update setup.py updated requests and singer python library version

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name='tap-mixpanel',
       py_modules=['tap_mixpanel'],
       install_requires=[
           'backoff==2.2.1',
-          'requests==2.32.4',
-          'singer-python==6.0.1',
+          'requests==2.34.2',
+          'singer-python==6.8.0',
           'jsonlines==1.2.0'
       ],
       entry_points='''
